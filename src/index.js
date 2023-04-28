e7ea14ccceba6641c877d87722516ce0b77b8b28
Simon Gingras <thegingman@gmail.com>
2023-04-28 08:03:54 -0400

    Add new JasperAdsTracker that relies on our telemetry (#5)

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import * as nrvideo from "newrelic-video-core";
 import Tracker from "./tracker";
-import AdsTracker from "./ads";
+import ShakaAdsTracker from "./shaka-ads";
+import JasperAdsTracker from "./jasper-ads";
 
 nrvideo.ShakaTracker = Tracker;
-nrvideo.ShakaAdsTracker = AdsTracker;
+nrvideo.ShakaAdsTracker = ShakaAdsTracker;
+nrvideo.JasperAdsTracker = JasperAdsTracker;
 
 module.exports = nrvideo;
